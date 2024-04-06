a9cb7471f7c217c08241ef9ea9a3136ee89c3a24
Faroeq <alves_1044@hotmail.com>
2024-04-06 11:30:23 +0200

    Swap to boring video, to avoid distraction

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ function App() {
       <div className="grid grid-cols-2 gap-4">
         <div className="p-4 instructions">
           <h2 className="text-2xl">Instructions</h2>
+          <p>Press play first before doing poses</p>
           <p> Open your hand = pausing </p>
           <p> Peacesign = fullscreen</p>
           <p> finger in front of the mouth = mute</p>
@@ -12,10 +13,10 @@ function App() {
           <h1 className="text-3xl font-bold underline">Video gestures</h1>
           <>
             <iframe
-              src="https://www.youtube.com/embed/HcqpanDadyQ"
-              title="What is Machine Learning?"
-              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
               className="w-full h-96"
+              src="https://www.youtube.com/embed/UNw-3mOEN-0"
+              title="How to read multi-tariff smart meter EMDI ES-10B"
+              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
               referrerPolicy="strict-origin-when-cross-origin"
               allowFullScreen={true}
             />
