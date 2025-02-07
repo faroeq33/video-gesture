32d6219bfe9f6ac1f4eb8c32bf3307815b232d81
Faroeq <alves_1044@hotmail.com>
2025-02-07 20:45:21 +0100

    Forgot part of refactoring

--- src/context/posecontext/PoseContext.tsx
+++ src/context/posecontext/PoseContext.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { PoseCollectionStream } from "../../utils/convertPosetoVector";
+import { PoseCollectionStream } from "../../types";
 
 interface PoseContextType {
   poseData: PoseCollectionStream | [];
