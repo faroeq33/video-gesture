18c0f6855be3069f21b3836b4ff27a2ef4d3e0e0
Faroeq <alves_1044@hotmail.com>
2025-04-08 12:52:36 +0200

    Add Vercel Speed Insights for performance monitoring

--- src/app.tsx
+++ src/app.tsx
@@ -1,9 +1,11 @@
 import Home from "@/pages/home";
 import { PoseProvider } from "@/context/posecontext/pose-context";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 
 export default function App() {
   return (
     <>
+      <SpeedInsights />
       <PoseProvider>
         <Home />
       </PoseProvider>
