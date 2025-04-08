0d6a58609c8c60573e3b801d491e2fd35141136b
Faroeq <alves_1044@hotmail.com>
2025-04-08 17:18:30 +0200

    Translate footer content

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -2,8 +2,8 @@ export default function Footer() {
   return (
     <div className="bg-purple">
       <p className="text-sm text-gray-500">
-        &copy; {new Date().getFullYear()} Een app voor mensen die muziek willen
-        bedienen d.m.v gebaren
+        &copy; {new Date().getFullYear()} An app for people who want to control
+        music through gestures
       </p>
     </div>
   );
