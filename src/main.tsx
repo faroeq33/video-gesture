b8721dcc5c52a4fa35312c6007920064bc7492f2
Faroeq <alves_1044@hotmail.com>
2025-04-09 16:28:42 +0200

    Enable strictmode

--- src/main.tsx
+++ src/main.tsx
@@ -2,11 +2,12 @@ import Home from "@/pages/home";
 import { PoseProvider } from "@/context/posecontext/pose-context";
 import ReactDOM from "react-dom/client";
 import "./assets/index.css";
+import { StrictMode } from "react";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
-  // <React.StrictMode>
-  <PoseProvider>
-    <Home />
-  </PoseProvider>
-  // </React.StrictMode><React.StrictMode>
+  <StrictMode>
+    <PoseProvider>
+      <Home />
+    </PoseProvider>
+  </StrictMode>
 );
