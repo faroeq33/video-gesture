4f42a9af279f33f597ff04ad713e4fe6f7056962
Faroeq <alves_1044@hotmail.com>
2025-04-12 22:00:24 +0200

    Add github link in footer

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,10 +1,24 @@
+import { Github } from "lucide-react";
+
 export default function Footer() {
   return (
-    <div className="flex justify-center items-center p-4 w-full">
-      <p className="text-sm text-center text-gray-500">
-        &copy; {new Date().getFullYear()} An app for people who want to control
-        music through gestures
-      </p>
+    <div className="flex items-center justify-center w-full p-4 text-sm text-center text-gray-500">
+      <ul className="thing">
+        <li className="text-sm text-center text-gray-500">
+          &copy; {new Date().getFullYear()} An app for people who want to
+          control music through gestures
+        </li>
+
+        <li className="text-gray-600">
+          <a
+            href="https://github.com/faroeq33/video-gesture"
+            className="underline"
+          >
+            <Github className="inline" />
+            https://github.com/faroeq33/video-gesture
+          </a>
+        </li>
+      </ul>
     </div>
   );
 }
