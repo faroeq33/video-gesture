602763179c11f161f4d5b8b82f562a7f193bc47e
Faroeq <alves_1044@hotmail.com>
2025-04-12 19:52:37 +0200

    Add styling for footer

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,7 +1,7 @@
 export default function Footer() {
   return (
-    <div className="bg-purple">
-      <p className="text-sm text-gray-500">
+    <div className="flex justify-center items-center p-4 w-full">
+      <p className="text-sm text-center text-gray-500">
         &copy; {new Date().getFullYear()} An app for people who want to control
         music through gestures
       </p>
