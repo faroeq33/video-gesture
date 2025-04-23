b1dbba637beef3303fadf1261eebee3e6fc4f4fb
Faroeq <alves_1044@hotmail.com>
2025-04-23 13:11:53 +0200

    fix: update main.tsx to use React.StrictMode and add eslint configuration

--- src/main.tsx
+++ src/main.tsx
@@ -2,12 +2,12 @@ import Home from "@/pages/home";
 import { PoseProvider } from "@/context/posecontext/pose-context";
 import ReactDOM from "react-dom/client";
 import "./assets/index.css";
-import { StrictMode } from "react";
+import React from "react";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
-  <StrictMode>
+  <React.StrictMode>
     <PoseProvider>
       <Home />
     </PoseProvider>
-  </StrictMode>
+  </React.StrictMode>
 );
